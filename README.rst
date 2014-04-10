6450e7a85098df5a1d218865e2f5ed5813bc5af0
Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
2014-04-11 01:25:12 +0200

    Initial commit

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-gelato
+Gelato
 ======
 
 An innovative kiosk system to manage ice cream sales in offices
---
+++ skeleton/admin.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+# admin.py
+#
+# Copyright (C) 2014 HES-SO//HEG Arc
+#
+# Author(s): Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
+#
+# This file is part of Gelato.
+#
+# Gelato is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gelato is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gelato. If not, see <http://www.gnu.org/licenses/>.
+
+# Stdlib imports
+
+# Core Django imports
+from django.contrib import admin
+
+# Third-party app imports
+
+# Gelato imports
+from .models import XXX
+
+
+class XXXAdmin(admin.ModelAdmin):
+    list_display = ('code', 'label', 'percentage', 'stock')
+
+
+admin.site.register(XXX, XXXAdmin)
