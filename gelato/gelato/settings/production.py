b7373d6ea7d1724aa2d393b7f54c012eaa6f5658
Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
2014-04-12 01:15:33 +0200

    Created configuration for production server

--- gelato/gelato/settings/production.py
+++ gelato/gelato/settings/production.py
@@ -20,6 +20,8 @@ def get_env_setting(setting):
         error_msg = "Set the %s env variable" % setting
         raise ImproperlyConfigured(error_msg)
 
+DEBUG = True
+
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
 ALLOWED_HOSTS = []
