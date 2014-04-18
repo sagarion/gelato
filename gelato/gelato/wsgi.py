ec76c36ee93b1aa36517a5280ef5f791f1709252
Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
2014-04-18 16:15:27 +0200

    Cleanup

--- gelato/gelato/wsgi.py
+++ gelato/gelato/wsgi.py
@@ -36,20 +36,10 @@ framework.
 
 """
 import os
+from django.core.handlers.wsgi import WSGIHandler
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gelato.settings.production")
-
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
-
-from django.core.handlers.wsgi import WSGIHandler
 
 
 class WSGIEnvironment(WSGIHandler):
